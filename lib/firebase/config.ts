9cd752ed7080ddbae71cf124161f3d20ee2e5307
SamuelRCrider <sam.crider@aol.com>
2024-06-11 15:45:09 -0400

    signup working, test complete

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -1,9 +1,9 @@
 import admin from "firebase-admin";
 
-import serviceAccount from "../../serviceAccountKey.json";
+const serviceAccount = require("../../serviceAccountKey.json");
 
 const app = admin.initializeApp({
-  credential: admin.credential.cert(`${serviceAccount}`),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const auth = app.auth();
